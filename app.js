60f6e7a3896b72c0683ddb3c714d302a94a3fafc
Johan Preus <jnpreus@gmail.com>
2021-06-12 17:00:48 -0500

    Node Callbacks and Error Handling

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const rect = require('./rectangle')
 
 function solveRect(l, w) {
     console.log(`Solving for rectangle with dimensions: ${l}, ${w}`)
-    if (l <= 0 || w <= 0) {
-        console.log(
-            `Rectangle dimensions must be greater than 0. Recieved: ${l}, ${w}`
-        )
-    } else {
-        console.log(`Area of rectangle: ${rect.area(l, w)}`)
-        console.log(`Perimerter of rectangle: ${rect.perimeter(l, w)}`)
-    }
+    
+    rect(l, w, (err, rectangle) => {
+        if(err){
+            console.log('ERROR:', err.message)
+        } else {
+            console.log(`Area of rectangle with dimensions ${l}, ${w}: ${rectangle.area()}`)
+            console.log(`Perimerter of rectangle with dimensions ${l}, ${w}: ${rectangle.perimeter()}`)
+        }
+    })
+    console.log('This statement is logged after the call to rect()');
 }
 
 solveRect(2, 4)
--- rectangle.js
+++ rectangle.js
@@ -1,3 +1,23 @@
 // exports is shorthand for module.exports
-exports.perimeter = (x, y) => 2 * (x * y)
-exports.area = (x, y) => x * y
+// shorthand can be used when assigning methods to exports,
+// but not when assigning to an entire function
+module.exports = (x, y, callback) => {
+    if (x <= 0 || y <= 0) {
+        callback(
+            new Error(
+                `Rectangle dimensions must be greater than 0. Recieved: ${x}, ${y}`
+            )
+        )
+    } else {
+        setTimeout(
+            () =>
+                // first argument null means no error
+                callback(null, {
+                    // x and y can be accessed because of closures
+                    perimeter: () => 2 * (x + y),
+                    area: () => x * y,
+                }),
+            2000
+        )
+    }
+}
